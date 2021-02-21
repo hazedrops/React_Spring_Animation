83a5d00038a19237a92f083915dfe3594452a24a
hazedrops <hazedrops@hotmail.com>
2021-02-21 15:19:20 -0800

    Simple React Transition Practice

---
+++ src/components/Component3.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Component3() {
+  return (
+    <div style={c3Style}>
+      <h1>Component 3</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae rem illo nostrum laudantium totam asperiores saepe necessitatibus, voluptatibus delectus voluptatem et nam laboriosam minima reiciendis blanditiis quae facilis odio?</p>
+    </div>
+  )
+}
+
+const c3Style = {
+  background: 'skyblue',
+  color: 'white',
+  padding: '1.5rem 1.5rem 5rem 1.5rem'
+}
